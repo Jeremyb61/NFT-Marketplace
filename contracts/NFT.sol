04b4aee8be6b8116412435da1a95e0c6a487fd02
Jeremy Billecci <jeremyb61@live.com>
2021-07-13 08:18:29 -0700

    first commit

---
+++ contracts/NFT.sol
@@ -0,0 +1,21 @@
+//contracts/NFT.sol
+//SPDX-License-Identifier: MIT OR Apache-2.0
+
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("Tokeroni's Tokens", "TOK") {
+        contractAddress = marketplaceAddress;
+    }
+}
+
